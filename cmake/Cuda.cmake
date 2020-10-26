c5d5a4c2dcf38b4d90f09e758d5266113240fbc7
spenco100 <spencers9478@gmail.com>
2020-10-26 14:07:56 -0500

    first push

--- cmake/Cuda.cmake
+++ cmake/Cuda.cmake
@@ -236,7 +236,7 @@ function(detect_cuDNN)
 
     # NEW
     # file(READ ${CUDNN_INCLUDE}/cudnn.h CUDNN_VERSION_FILE_CONTENTS)
-    file(READ ${CUDNN_INCLUDE}/cudnn_version.h CUDNN_VERSION_FILE_CONTENTS)
+    file(READ {CUDNN_INCLUDE}/cudnn_version.h CUDNN_VERSION_FILE_CONTENTS)
 
     # cuDNN v3 and beyond
     string(REGEX MATCH "define CUDNN_MAJOR * +([0-9]+)"
